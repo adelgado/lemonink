fa5fe25e50d9f35ff2e205565195453db89f5a9b
Pablo Botelho <pablo.bcb@gmail.com>
2012-11-10 07:10:05 -0200

    database

--- src/app.js
+++ src/app.js
@@ -1,43 +1,157 @@
+(function(){
+	var express = require('express')
+	, http    = require('http')
+	, path    = require('path')
+	, redis   = require('redis')
+	, uuid    = require('node-uuid')
 
-/**
- * Module dependencies.
- */
+	var app = express()
 
-var express = require('express')
-  , routes = require('./routes')
-  , index = require('./routes/index')
-  , topic = require('./routes/topic')
-  , http = require('http')
-  , path = require('path')
+	app.use(function (req, res, next){
+		res.jsonSuccess = function (httpCode, data){
+			res.json(httpCode, {success : true, data: data})
+		} 
+		res.jsonError = function(httpCode, data) {
+			res.json(httpCode, {success: false, err: errMsg})	
+		}
 
-var app = express()
+		next()
+	})
 
-var routes = {}
-routes.index = index
-routes.topic = topic
+	var client = redis.createClient()
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000)
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'hjs')
-  app.use(express.favicon())
-  app.use(express.logger('dev'))
-  app.use(express.bodyParser())
-  app.use(express.methodOverride())
-  app.use(app.router)
-  app.use(express.static(path.join(__dirname, 'public')))
-})
+	var db = {
+		createTopic: function(topic, callback) {
+			var id = uuid.v4()
+			client.hset("topic", id, JSON.stringify(topic), function(err, reply) {
+				client.quit()
 
-app.configure('development', function(){
-  app.use(express.errorHandler())
-})
+				if (reply === 1) {
+					callback(null, id)
+				}
 
-app.get  ('/', function(req, res) { res.redirect('/topic') })
-app.get  ('/topic',     routes.topic.list)
-app.get  ('/topic/:id', routes.topic.view)
-app.post ('/topic',     routes.topic.create)
-app.post ('/topic/:id', routes.topic.reply)
+				if (reply === 0) {
+					callback(err)
+				}
+			})
+		},
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'))
-})
+		getAllTopics : function(callback) {
+			client.hgetall("topic", function(err, topics) {
+				if (err) {
+					callback(err, {success: false})
+				}
+
+				var parsed_topics = []
+				for (var id in topics) {
+					var topic = JSON.parse(topics[id])
+					topic.id = id
+					parsed_topics.push(topic)
+				}
+
+				callback(null, parsed_topics)
+			})
+		}
+	}
+
+	//HTTP response codes
+	var HTTP = {
+		OK              : 200,
+		CREATED         : 201,
+		BAD_REQUEST     : 400,
+		NOT_IMPLEMENTED : 501
+	}
+
+	app.configure(function(){
+		app.set('port', process.env.PORT || 3000)
+		app.set('views', __dirname + '/views')
+		app.set('view engine', 'hjs')
+		app.use(express.favicon())
+		app.use(express.logger('dev'))
+		app.use(express.bodyParser())
+		app.use(express.methodOverride())
+		app.use(app.router)
+		app.use(express.static(path.join(__dirname, 'public')))
+	})
+
+	app.configure('development', function(){
+		app.use(express.errorHandler())
+	})
+
+
+	function validateFields(fields) {
+		return function(req, res, next) {
+			fields.map(function(field) {
+				if (typeof(req.body[field]) === 'undefined' ||
+					req.body[field] === '') {
+					res.json(HTTP.BAD_REQUEST, { success : false })
+				}
+			})
+
+			next()
+		}
+	}
+
+
+	app.get  ('/', function(req, res) { res.redirect('/topic') })
+
+
+	// Creates a new discussion topic with one post associated with it
+	app.post ('/topic',
+		validateFields(['title', 'author', 'message']),
+		function(req, res) {
+			var topic = {
+				title : req.body.title,
+				author : req.body.author,
+				message : req.body.message
+			}
+
+			db.createTopic(topic, function(err, id) {
+				if (err) {
+					res.jsonError(HTTP.BAD_REQUEST, { error : err })
+				}
+
+				res.header('Location', '/topic/' + id)
+				res.jsonSuccess(HTTP.CREATED, { id : id })
+			})
+		}
+	)
+
+	
+	// Gets all the topics
+	app.get  ('/topic',
+		function (req, res) {
+			db.getAllTopics(function(err, topics) {
+				if (err) {
+					//
+				}
+
+				res.render('topic/list', {
+					topics: topics
+				})
+			})
+		}
+	)
+
+	// Gets a topic with all the posts associated with it
+	app.get  ('/topic/:id',
+		function(req, res) {
+			res.send(HTTP.NOT_IMPLEMENTED)
+		}
+	)
+
+
+	// Replies to a specific topic
+	app.post ('/topic/:id',
+		validateFields(['author', 'message']),
+
+		function(req, res) {
+			res.send(HTTP.NOT_IMPLEMENTED)
+		}
+	)
+
+
+	http.createServer(app).listen(app.get('port'), function(){
+		console.log("Express server listening on port " + app.get('port'))
+	})
+})()
---
+++ src/db.js
@@ -0,0 +1,37 @@
+module.export = (function () { 
+	var client = require('redis').createServer()
+
+	return {
+		createTopic: function(topic, callback) {
+		var id = uuid.v4()
+			client.hset("topic", id, JSON.stringify(topic), function(err, reply) {
+				client.quit()
+
+				if (reply === 1) {
+					callback(null, id) 
+				}   
+
+			if (reply === 0) {
+				callback(err)
+			}   
+		})  
+	},  
+
+		getAllTopics : function(callback) {
+			client.hgetall("topic", function(err, topics) {
+				if (err) {
+					callback(err, {success: false})
+				}   
+
+				var parsed_topics = []
+				for (var id in topics) {
+					var topic = JSON.parse(topics[id])
+				topic.id = id
+				parsed_topics.push(topic)
+				}   
+
+			callback(null, parsed_topics)
+			})  
+		}   
+	}
+})()
